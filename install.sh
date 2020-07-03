10d2fcdb8e6ccc54c739fe6980d05632af931c1f
Brahim Machkouri <brahimmachkouri@users.noreply.github.com>
2020-07-03 22:32:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ wget https://github.com/FOGProject/fogproject/archive/master.tar.gz
 tar -zxvf master.tar.gz
 file="fogproject-master/lib/common/functions.sh"
 sed -i "s/downloadfiles()/downloadfiles2()/g" $file
-downloadfunction=$(<downloadfiles.sh)
-echo "$downloadfunction" >> $file
+cat downloadfiles.sh >> $file
 cd fogproject-master/bin
 ./installfog.sh
