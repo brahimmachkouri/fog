0b3d51b60b2792dd594fe9537c9d5f00489727d5
Brahim Machkouri <brahimmachkouri@users.noreply.github.com>
2020-07-03 22:34:54 +0200

    Update downloadfiles.sh

--- downloadfiles.sh
+++ downloadfiles.sh
@@ -3,20 +3,22 @@ downloadfiles() {
     [[ ! -d ../tmp/ ]] && mkdir -p ../tmp/ >/dev/null 2>&1
     cwd=$(pwd)
     cd ../tmp/
-
     urls=( "https://github.com/brahimmachkouri/fog/blob/master/binaries/bzImage" "https://github.com/brahimmachkouri/fog/blob/master/binaries/bzImage32" "https://github.com/brahimmachkouri/fog/blob/master/binaries/init.xz" "https://github.com/brahimmachkouri/fog/blob/master/binaries/init_32.xz" "https://github.com/brahimmachkouri/fog/blob/master/binaries/SmartInstaller.exe" "https://github.com/brahimmachkouri/fog/blob/master/binaries/FOGService.msi")
     for url in "${urls[@]}"
     do
         filename=$(basename -- "$url")
         baseurl=$(dirname -- "$url")
-        curl --silent -kOL $url
+        curl --silent -kOL $url >>$workingdir/error_logs/fog_error_${version}.log 2>&1
     done
     echo "Done"
+    errorStat $?
+    copypath="./"
     dots "Copying binaries to destination paths"
-    cp -vf bzImage ${webdirdest}/service/ipxe/ 
-    cp -vf bzImage32 ${webdirdest}/service/ipxe/ 
-    cp -vf init.xz ${webdirdest}/service/ipxe/ 
-    cp -vf init_32.xz ${webdirdest}/service/ipxe/ 
-    cp -vf FOGService.msi ${copypath}SmartInstaller.exe ${webdirdest}/client/ 
+    cp -vf ${copypath}bzImage ${webdirdest}/service/ipxe/ >>$workingdir/error_logs/fog_error_${version}.log 2>&1 || errorStat $?
+    cp -vf ${copypath}bzImage32 ${webdirdest}/service/ipxe/ >>$workingdir/error_logs/fog_error_${version}.log 2>&1 || errorStat $?
+    cp -vf ${copypath}init.xz ${webdirdest}/service/ipxe/ >>$workingdir/error_logs/fog_error_${version}.log 2>&1 || errorStat $?
+    cp -vf ${copypath}init_32.xz ${webdirdest}/service/ipxe/ >>$workingdir/error_logs/fog_error_${version}.log 2>&1 || errorStat $?
+    cp -vf ${copypath}FOGService.msi ${copypath}SmartInstaller.exe ${webdirdest}/client/ >>$workingdir/error_logs/fog_error_${version}.log 2>&1
+    errorStat $?
     cd $cwd
 }
